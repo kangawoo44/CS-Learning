566616acc478f67e9a71fb9bd536ebf4ddb3bb11
Justin Kang <wookang4@gmail.com>
2020-08-13 21:58:23 -0700

    matrix

--- Javascript/myLibrary.js
+++ Javascript/myLibrary.js
@@ -125,3 +125,26 @@ var singleNumber = function(nums) {
     nums.forEach(num => a ^= num)
     return a
 };
+
+//transpose 2D matrix
+const transpose = (matrix) => {
+    let [row] = matrix
+    return row.map((value, column) => matrix.map(row => row[column]))
+}
+
+// /*
+//  * clockwise rotation
+//  * first reverse up to down, then swap the symmetry 
+//  * 1 2 3     7 8 9     7 4 1
+//  * 4 5 6  => 4 5 6  => 8 5 2
+//  * 7 8 9     1 2 3     9 6 3
+// */
+matrix.reverse()
+for (let i = 0; i < matrix.length; i++) { //for each row
+    for (let j = i + 1; j < matrix[i].length; j++) { //for each column
+        let temp = matrix[i][j]
+        matrix[i][j] = matrix[j][i]
+        matrix[j][i] = temp
+    }
+}
+
