2abb58ff540f270d9d1ed5a9033c709651255bb4
Woo Kang <wookang4@gmail.com>
2021-07-14 00:35:22 -0700

    lift state up to board, add player O, and add calculateWinner function

--- React/React-Tutorial/my-app/src/index.js
+++ React/React-Tutorial/my-app/src/index.js
@@ -2,46 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       /*Notice how with onClick={() => alert('click')}, we’re passing a function as the onClick prop.
+//       React will only call this function after a click.
+//       Forgetting () => and writing onClick={alert('click')} is a common mistake, 
+//       and would fire the alert every time the component re-renders.*/
+//       // <button className="square" onClick={() => alert('click')}>
+//       <button className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
   /*In JavaScript classes, you need to always call super when defining the constructor of a subclass. 
   All React component classes that have a constructor should start with a super(props) call.*/
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      /*Notice how with onClick={() => alert('click')}, we’re passing a function as the onClick prop.
-      React will only call this function after a click.
-      Forgetting () => and writing onClick={alert('click')} is a common mistake, 
-      and would fire the alert every time the component re-renders.*/
-      // <button className="square" onClick={() => alert('click')}>
-      <button className="square"
-        onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
     //   return <Square />;
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -88,3 +105,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
