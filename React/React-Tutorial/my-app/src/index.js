0c0fe2aadbff0b3f0a26ee7684a9555df4e9cbad
Woo Kang <wookang4@gmail.com>
2021-07-13 22:41:02 -0700

    format

--- React/React-Tutorial/my-app/src/index.js
+++ React/React-Tutorial/my-app/src/index.js
@@ -3,82 +3,88 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    /*In JavaScript classes, you need to always call super when defining the constructor of a subclass. 
-    All React component classes that have a constructor should start with a super(props) call.*/
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+  /*In JavaScript classes, you need to always call super when defining the constructor of a subclass. 
+  All React component classes that have a constructor should start with a super(props) call.*/
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
 
-    render() {
-      return (
-          /*Notice how with onClick={() => alert('click')}, we’re passing a function as the onClick prop.
-          React will only call this function after a click.
-          Forgetting () => and writing onClick={alert('click')} is a common mistake, 
-          and would fire the alert every time the component re-renders.*/
-        // <button className="square" onClick={() => alert('click')}>
-        <button className="square" 
-        onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
-    }
+  render() {
+    return (
+      /*Notice how with onClick={() => alert('click')}, we’re passing a function as the onClick prop.
+      React will only call this function after a click.
+      Forgetting () => and writing onClick={alert('click')} is a common mistake, 
+      and would fire the alert every time the component re-renders.*/
+      // <button className="square" onClick={() => alert('click')}>
+      <button className="square"
+        onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  renderSquare(i) {
     //   return <Square />;
-      return <Square value={i} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+    return <Square value={i} />;
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
-    }
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
