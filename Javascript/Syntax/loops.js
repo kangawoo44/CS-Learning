c1d76ff556dd4aae5254b8f0a1ce6252bcc05f09
Woo Kang <wookang4@gmail.com>
2021-08-19 15:02:28 -0700

    notes

--- Javascript/Syntax/loops.js
+++ Javascript/Syntax/loops.js
@@ -8,6 +8,10 @@ let array1 = ['a', 'b', 'c']
 for (const element of array1) {
   console.log('element', element)
 }
+// if you want the index:
+for (const [i, element] of array1.entries()) {
+  console.log('index of array1', i)
+}
 
 // Map is iterable unlike objects, for hashmaps
 let map1 = new Map()
@@ -25,9 +29,15 @@ for (const key of map1.keys()) {
 // objects are not iterable, use for..in - to iterate over all enemuerable properties of an object
 let object1 = {name: 'woo', num: 4}
 for (const key in object1) {
-  console.log('object properties', key, object1[key])
+  console.log('object key value', key, object1[key])
+}
+// or just use Object.entries() and for...of
+for (const [key, value] of Object.entries(object1)) {
+  console.log('object entries', key, value)
 }
 
 // forEach loops are just for arrays
 array1.forEach(element => console.log(element))
 array1.forEach((element, index) => {console.log(element, index)})
+
+str.split('').forEach((char, i) => console.log(char, i)) //O(n) for split('')
