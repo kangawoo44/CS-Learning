c8c833a25238b0dfd19632342911ae0a36311fd7
Woo Kang <wookang4@gmail.com>
2021-08-19 09:39:21 -0700

    org

--- Javascript/Syntax/object.js
+++ Javascript/Syntax/object.js
@@ -5,23 +5,28 @@ let object_x = {
 }
 // method #1 just try to access it directly
 let x_name = object_x.name
-console.log(x_name)
+console.log('x_name', x_name)
 x_name = object_x['name']
-console.log(x_name)
+console.log('x_name', x_name)
+// if it doesn't exist you'll get undefined
+x_name = object_x.something
+console.log('x_name', x_name)
+x_name = object_x['something']
+console.log('x_name', x_name)
 
 // method #2: use 'in'
 let hasKey = 'name' in object_x
-console.log(hasKey)
+console.log('hasKey', hasKey)
 
 // method #3: hasOwnProperty()
 hasKey = object_x.hasOwnProperty('name')
-console.log(hasKey)
+console.log('hasKey', hasKey)
 
 
 // How to check against object values? O(n)
 // Step 1. get array of object's values
 let x_values = Object.values(object_x) //O(n)
-console.log(x_values)
+console.log('x_values', x_values)
 // Step 2. loop through the array of values
 let hasValue = x_values.includes('woo') //O(n)
-console.log(hasValue)
+console.log('hasValue', hasValue)
