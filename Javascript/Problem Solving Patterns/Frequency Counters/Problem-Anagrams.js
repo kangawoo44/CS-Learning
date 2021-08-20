103313c6c8e12cfca687da59fc8b487fc621c1d0
Woo Kang <wookang4@gmail.com>
2021-08-20 13:14:44 -0700

    add problem-anagram

---
+++ Javascript/Problem Solving Patterns/Frequency Counters/Problem-Anagrams.js
@@ -0,0 +1,13 @@
+/*
+Given two strings, write a function to determine if the second string is an anagram of the first.
+An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+Example:
+validAnagram('', '') // ture
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // ture
+validAnagram('rat', 'car') // false
+validAnagram('awesome', 'awesom') // false
+validAnagram('qwerty', 'qeywrt') // true
+validAnagram('texttwisttime', 'timetwisttext') // ture
+*/
