4a04380ef9d48c02275c49f8e673aa1036ae77f4
Woo Kang <wookang4@gmail.com>
2021-08-22 16:35:14 -0700

    solve

--- Javascript/Problem Solving Patterns/Frequency Counters/Problem-sameFrequency.js
+++ Javascript/Problem Solving Patterns/Frequency Counters/Problem-sameFrequency.js
@@ -12,3 +12,25 @@
  * sameFrequency(22,222) // false
  */
 
+function sameFrequency(num1, num2) {
+  num1 = num1.toString();
+  num2 = num2.toString();
+  if (!(num1.length === num2.length)) return false;
+  let num1_map = {};
+  for (const char of num1) {
+    num1_map[char] = num1_map[char] ? num1_map[char] + 1 : 1;
+  }
+  for (const char of num2) {
+    if (num1_map[char]) {
+      num1_map[char] = num1_map[char] - 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(sameFrequency(182,281)) // true
+console.log(sameFrequency(34,14)) // false
+console.log(sameFrequency(3589578,5879385)) // true
+console.log(sameFrequency(22,222)) // false
