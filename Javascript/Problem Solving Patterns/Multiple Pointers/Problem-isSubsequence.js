3f5d9e6385b76308043e16fa15425e22a0f6fb5d
Woo Kang <wookang4@gmail.com>
2021-08-23 21:52:45 -0700

    solve

--- Javascript/Problem Solving Patterns/Multiple Pointers/Problem-isSubsequence.js
+++ Javascript/Problem Solving Patterns/Multiple Pointers/Problem-isSubsequence.js
@@ -16,3 +16,33 @@
  * space - O(1)
  */
 
+function isSubsequence(str1, str2) {
+  //type
+  if (!str1 || !str2 
+    || typeof str1 !== 'string' || typeof str2 !== 'string' 
+    || !str1.length || str2.length < str1.length) {
+      console.log('false');
+      return false;
+    }
+
+  let matchCount = 0;
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if (str1[i] === str2[j]) {
+        i++;
+        matchCount++;
+      }
+      if (matchCount === str1.length) {
+        console.log('true');
+        return true;
+      }
+    }
+  }
+  console.log('false');
+  return false;
+}
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
