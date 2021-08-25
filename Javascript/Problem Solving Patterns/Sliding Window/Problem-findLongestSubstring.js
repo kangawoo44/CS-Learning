221c617b4d85e0d585c6cc7b382ce67bcb62b268
Woo Kang <wookang4@gmail.com>
2021-08-25 09:11:53 -0700

    solve minSubArryLen

--- Javascript/Problem Solving Patterns/Sliding Window/Problem-findLongestSubstring.js
+++ Javascript/Problem Solving Patterns/Sliding Window/Problem-findLongestSubstring.js
@@ -6,39 +6,59 @@
  * Time - O(n)
  */
 
-function findLongestSubstring(str) { //time O(n), space O(n)
-  if (str.length <= 1) {
-    console.log(str.length);
-    return str.length;
-  }
-  //loop over entire string
-  //make a hash map to store each character
-  //for each character, add into map if it doesn't exist
-    //count++ and store into variable if it's greater than the max length found so far
-  //if it exists, change the position to 1 + position of previous duplicate char
-    //reset hashmap
-  //if the rest of the string length is less than the current max length found, return max length
-  let maxLength = 0;
-  let charMap = new Map();
-  let counter = 0;
+// function findLongestSubstring(str) { //time O(n), space O(n)
+//   if (str.length <= 1) {
+//     console.log(str.length);
+//     return str.length;
+//   }
+//   //loop over entire string
+//   //make a hash map to store each character
+//   //for each character, add into map if it doesn't exist
+//     //count++ and store into variable if it's greater than the max length found so far
+//   //if it exists, change the position to 1 + position of previous duplicate char
+//     //reset hashmap
+//   //if the rest of the string length is less than the current max length found, return max length
+//   let maxLength = 0;
+//   let charMap = new Map();
+//   let counter = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (!charMap.has(str[i])) {
+//       charMap.set(str[i], i);
+//       counter++;
+//       maxLength = counter > maxLength ? counter : maxLength;
+//     } else {
+//       i = charMap.get(str[i]);
+//       charMap = new Map();
+//       counter = 0;
+//       let charsLeftOver = str.length - i - 1;
+//       if (charsLeftOver < maxLength) {
+//         console.log(maxLength);
+//         return maxLength;
+//       }
+//     }
+//   }
+//   console.log(maxLength);
+//   return maxLength;
+// }
+
+//a better solution
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+ 
   for (let i = 0; i < str.length; i++) {
-    if (!charMap.has(str[i])) {
-      charMap.set(str[i], i);
-      counter++;
-      maxLength = counter > maxLength ? counter : maxLength;
-    } else {
-      i = charMap.get(str[i]);
-      charMap = new Map();
-      counter = 0;
-      let charsLeftOver = str.length - i - 1;
-      if (charsLeftOver < maxLength) {
-        console.log(maxLength);
-        return maxLength;
-      }
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
     }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
   }
-  console.log(maxLength);
-  return maxLength;
+  console.log(longest);
+  return longest;
 }
 
 findLongestSubstring('') // 0
