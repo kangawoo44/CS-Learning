767c2dc32273c49c268403bd0ca66973eb33dd63
Woo Kang <wookang4@gmail.com>
2021-10-03 16:53:02 -0700

    add pseudo code to naive string search

---
+++ Javascript/Data Structures & Algorithms/Algorithms/Problems/Search-naiveStringSearch.js
@@ -0,0 +1,19 @@
+/**
+ * naiveStringSearch
+ * 
+ * Pseudo Code:
+ * loop over the longer string
+ * loop over the shorter string
+ * if the characters don't match, break out of the inner loop
+ * if the characters do match, keep going
+ * if you complete the inner loop and find a match, increment the count of matches
+ * return the count
+ */
+
+let naiveStringSearch = (longStr, shortStr) => {
+
+  return numberOfMatches;
+}
+
+console.log(naiveStringSearch("", ""));
+console.log(naiveStringSearch("hello my name is helion. people often say he's a cool guy.", "he")); // 3
