790f0187c1e6e34f6c3a5a6b56663df2a7dc0b2f
Woo Kang <wookang4@gmail.com>
2021-10-03 17:18:24 -0700

    add naive search and bubble sort code

---
+++ Javascript/Data Structures & Algorithms/Algorithms/Problems/Search-bubbleSort.js
@@ -0,0 +1,39 @@
+/**
+ * Bubble Sort
+ * 
+ * largest values "bubble" to the top (i.e. the end of an array) each iteration
+ * keep comparing from beginning to end, two consecutive items and swap if the left is bigger than right
+ */
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr){
+  for(var i = arr.length; i > 0; i--){
+    for(var j = 0; j < i - 1; j++){
+      console.log(arr, arr[j], arr[j+1]);
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;         
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
--- Javascript/Data Structures & Algorithms/Algorithms/Problems/Search-naiveStringSearch.js
+++ Javascript/Data Structures & Algorithms/Algorithms/Problems/Search-naiveStringSearch.js
@@ -11,9 +11,25 @@
  */
 
 let naiveStringSearch = (longStr, shortStr) => {
-
+  //loop through the longStr
+  //if a character match is found, loop through the shortStr and see if every character matches. During this loop the longStr will only temporarily change its index position.
+  //if the whole shortStr match is found, increment the numberOfMatches counter. Continue looping through longStr from where it was left off (after first char match if no complete match to shortStr was found).
   return numberOfMatches;
 }
 
 console.log(naiveStringSearch("", ""));
 console.log(naiveStringSearch("hello my name is helion. people often say he's a cool guy.", "he")); // 3
+
+////////////////////////////////////////////////////////////////////////////
+function naiveSearch(long, short){
+  var count = 0;
+  for(var i = 0; i < long.length; i++){
+      for(var j = 0; j < short.length; j++){
+         if(short[j] !== long[i+j]) break;
+         if(j === short.length - 1) count++;
+      }
+  }
+  return count;
+}
+
+naiveSearch("lorie loled", "lol")
