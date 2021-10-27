1ffd0f27dc7bbab379a2ebba152c6d0f53ef0af5
Woo Kang <wookang4@gmail.com>
2021-10-26 23:40:47 -0700

    react learning

--- React/Pokédex/my-pokedex-app/src/index.js
+++ React/Pokédex/my-pokedex-app/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//   return <div>Latitude: {}</div>;
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({lat: position.coords.latitude});
+        console.log(position);
+      },
+      err => {
+        this.setState({errorMessage: err.message})
+        console.log(err);
+      }
+    );
+  }
+
+  componentDidMount() {
+    console.log("Component was rendered to the screen")
+  }
+
+  componentDidUpdate() {
+    console.log("Component was updated")
+  }
+
+  render() {
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
