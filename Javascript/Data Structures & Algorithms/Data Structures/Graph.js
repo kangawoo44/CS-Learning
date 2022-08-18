5e7365974a6c8c478d44ce8dcba520127d222b34
Woo Kang <wookang4@gmail.com>
2022-08-17 22:18:46 -0700

    add undirected unweighted graph implementation

--- Javascript/Data Structures & Algorithms/Data Structures/Graph.js
+++ Javascript/Data Structures & Algorithms/Data Structures/Graph.js
@@ -3,54 +3,52 @@
  * graphs can be represented in 3 ways: edge list, adjacent list, adjacent matrix
  */
 
-class Graph { 
-  constructor() { 
+class Graph {
+  constructor() {
     this.numberOfNodes = 0;
-    this.adjacentList = {
-    }; 
-  } 
-  addVertex(node)  { 
-  } 
-  addEdge(node1, node2) { 
-    //undirected Graph 
-  } 
-  showConnections() { 
-    const allNodes = Object.keys(this.adjacentList); 
-    for (let node of allNodes) { 
-      let nodeConnections = this.adjacentList[node]; 
-      let connections = ""; 
-      let vertex;
-      for (vertex of nodeConnections) {
-        connections += vertex + " ";
-      } 
-      console.log(node + "-->" + connections); 
-    } 
-} 
-} 
+    this.adjacentList = {};
+  }
+  addVertex(node) {
+    this.adjacentList[`${node}`] = [];
+    this.numberOfNodes++;
+  }
+  addEdge(node1, node2) {
+    //undirected Graph
+    for (const [node, list] of Object.entries(this.adjacentList)) {
+      if (node === node1) list.push(node2);
+      if (node === node2) list.push(node1);
+    }
+  }
+  showConnections() {
+    for (const [node, list] of Object.entries(this.adjacentList)) {
+      console.log(`${node} --> ${list.join(' ').split(',')}`);
+    }
+  }
+}
 
 const myGraph = new Graph();
-myGraph.addVertex('0');
-myGraph.addVertex('1');
-myGraph.addVertex('2');
-myGraph.addVertex('3');
-myGraph.addVertex('4');
-myGraph.addVertex('5');
-myGraph.addVertex('6');
-myGraph.addEdge('3', '1'); 
-myGraph.addEdge('3', '4'); 
-myGraph.addEdge('4', '2'); 
-myGraph.addEdge('4', '5'); 
-myGraph.addEdge('1', '2'); 
-myGraph.addEdge('1', '0'); 
-myGraph.addEdge('0', '2'); 
-myGraph.addEdge('6', '5');
+myGraph.addVertex("0");
+myGraph.addVertex("1");
+myGraph.addVertex("2");
+myGraph.addVertex("3");
+myGraph.addVertex("4");
+myGraph.addVertex("5");
+myGraph.addVertex("6");
+myGraph.addEdge("3", "1");
+myGraph.addEdge("3", "4");
+myGraph.addEdge("4", "2");
+myGraph.addEdge("4", "5");
+myGraph.addEdge("1", "2");
+myGraph.addEdge("1", "0");
+myGraph.addEdge("0", "2");
+myGraph.addEdge("6", "5");
 
-myGraph.showConnections(); 
+myGraph.showConnections();
 //Answer:
-// 0-->1 2 
-// 1-->3 2 0 
-// 2-->4 1 0 
-// 3-->1 4 
-// 4-->3 2 5 
-// 5-->4 6 
+// 0-->1 2
+// 1-->3 2 0
+// 2-->4 1 0
+// 3-->1 4
+// 4-->3 2 5
+// 5-->4 6
 // 6-->5
