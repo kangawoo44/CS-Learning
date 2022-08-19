7c666a645fee9e06b9f6d9129acf3a765772e290
Woo Kang <wookang4@gmail.com>
2022-08-19 11:40:12 -0700

    add console logs

--- Javascript/Data Structures & Algorithms/Algorithms/Problems/Sort-insertionSort.js
+++ Javascript/Data Structures & Algorithms/Algorithms/Problems/Sort-insertionSort.js
@@ -5,14 +5,17 @@
  */
 
 let insertionSort = (arr) => { //O(n^2), but if the array is nearly sorted, this sort performs well.
+  console.log("o", arr)
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
     for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j+1] = arr[j];
+      console.log("1", arr)
     }
     arr[j+1] = currentVal;
+    console.log("2", arr)
   }
-  console.log(arr);
+  console.log("f", arr);
   return arr;
 }
 
